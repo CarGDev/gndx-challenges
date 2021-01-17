fccdbb4365e96227590ad0be82da0e5745d231f8
Carlos Gutierrez <ingecarlos.gutierrez@gmail.com>
2021-01-17 17:46:12 -0600

    Creating 404 page not found

--- frontend/src/app/home/components/home/home.component.ts
+++ frontend/src/app/home/components/home/home.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  lost: string = '404!'
+  title: string = 'UH OH! Seems You are lost'
+  description: string = 'The page you are looking for does not exist. How you got here is a real mistery. But do not worry, just click the button below to go back!'
   ngOnInit(): void {
   }
 
